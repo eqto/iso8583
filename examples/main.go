48df03845b04f31c9444673debe8ec621053db6f
tuxer <e.yulian@gmail.com>
2020-11-09 00:56:36 +0700

    comment example

--- examples/main.go
+++ examples/main.go
@@ -10,15 +10,12 @@ import (
 func main() {
 	//create 0800 request
 	sign := &message.Signon{NetworkCode: 1}
-	builder := iso8583.NewBuilder(`0800`, 7, 11, 48, 70)
+	builder := iso8583.NewBuilder(`0800`, 7, 11, 70)
 	req, _ := builder.New(sign)
-
 	println(fmt.Sprintf(`Sign Request: %s`, req))
+
+	//parse 0800 request
 	resp, _ := iso8583.Parse(req.Bytes())
 	println(fmt.Sprintf("== Parsed Sign Request ==\n%s", resp.Dump()))
 	println(`== End Request ==`)
-
-	// t.Log(msg.JSON().ToFormattedString())
-	// t.Log(string(msg.Bytes()))
-
 }
